bb42e5dbb6f2c1ef7b80d2b15e45a0ebab79e5b8
Vladimir Humeniuk <vg6962@gmail.com>
2017-02-20 18:38:06 +0200

    Prerelease

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,8 @@
-console.log("Love FontEnd");
+function GulpBoilerplate() {
+    var Love = 'Love FrontEnd';
+    var GB = 'Gulp Boilerplate';
+
+    console.log(GB + " + " + Love + " = real feelings" );
+}
+
+GulpBoilerplate();
