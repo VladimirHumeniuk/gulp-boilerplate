60c38d241925108ee690d09055265aaf7bd408a9
VHumeniuk <vg6962@gmail.com>
2017-04-09 02:41:45 +0300

    Add es6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp'); 
+let gulp = require('gulp'); 
 	server = require('gulp-server-livereload'); // сервер
 	sass = require('gulp-sass'); // компилирует sass в css 
 	prefix = require('gulp-autoprefixer'); // проставляет префиксы для кроссбраузерности
@@ -12,26 +12,26 @@ var gulp = require('gulp');
 
 
 // запускаем сервер
-gulp.task('start', function(){
+gulp.task('start', () =>
 	gulp.src('app') // указываем папку, в которой мы делаем сайт
 		.pipe(server({
 			open: true, // сразу открываем браузер
 			livereload: true // поднимаем сервер
 		}))
-});
+);
 
 // готовим css
-gulp.task('style', function(){
+gulp.task('style', () =>
 	gulp.src('app/sass/**/*.sass') //указывая такой путь, мы говорим плагину компилировать все файлы в папке app/sass
 		.pipe(sass().on('error', sass.logError)) // выводим ошибки компиляции в консоль при их возникновении
 		.pipe(prefix({
 			versions: ['last 20 versions'] // проставляем префиксы для последних 20 версий браузеров
 		}))
 		.pipe(gulp.dest('app/css')) //исходящяя папка для css файлов
-});
+);
 
 // уменьшаем картинки
-gulp.task('images', function () {
+gulp.task('images', () =>
     gulp.src('./app/img/**/*') //указывая такой путь, мы говорим плагину компилировать все файлы в папке app/img
     	.pipe(cleanDest('build/img')) 
         .pipe(imagemin({
@@ -39,19 +39,19 @@ gulp.task('images', function () {
 			quality: 50,
 			smooth: 30
         }))
-        .pipe(gulp.dest('build/img')); // папка, в которую будут поступать оптимизированные картинки
-});
+        .pipe(gulp.dest('build/img')) // папка, в которую будут поступать оптимизированные картинки
+);
 
 // переносим шрифты в билд
-gulp.task('fonts', function() {
-  return gulp.src(('./app/fonts/**/*'), ['*.eot','*.svg','*.ttf','*.woff','*.woff2'])  
-    .pipe(gulp.dest('build/fonts'));                                     
-});
+gulp.task('fonts', () =>
+  gulp.src(('./app/fonts/**/*'), ['*.eot','*.svg','*.ttf','*.woff','*.woff2'])  
+    .pipe(gulp.dest('build/fonts'))                                    
+);
 
 // отслеживаем изменения в sass файлах чтобы сразу компилировать их в css
-gulp.task('watch', function(){
+gulp.task('watch', () =>
 	gulp.watch('app/sass/**/*.sass', ['style'])
-});
+);
 
 // смотрим за sass файлами, чтобы каждый раз не компилировать в сss все файлы, а только измененные
 gulp.task('changed', () =>
@@ -62,13 +62,13 @@ gulp.task('changed', () =>
 );
 
 // собираем наш проект
-gulp.task("build", ['images'] ,function(){
+gulp.task("build", ['images', 'fonts'], () =>
 	gulp.src("app/*.html")
 		.pipe(useref())
 		.pipe(gulpif("*.css", csso())) // говорим, что для css файлов применять плагин csso
 		.pipe(gulpif("*.js", uglify())) // а для js файлов плагин uglify
 		.pipe(gulp.dest('build')) // указываем корневую папку для билда
-});
+);
 
 // дефолтный таск
 gulp.task('default', ['start', 'watch', 'changed']);
